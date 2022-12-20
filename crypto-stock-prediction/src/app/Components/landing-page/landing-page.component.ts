938db1031997a928ec86fa9ed24a5723f6b63c1e
MaximeLombard <maxime.lombard@live.com.mx>
2022-12-20 21:54:23 +0100

    mobile: landing page complete

--- crypto-stock-prediction/src/app/Components/landing-page/landing-page.component.ts
+++ crypto-stock-prediction/src/app/Components/landing-page/landing-page.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface TableData {
+  content: string;
+  className: string;
+}
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  tableContent: Array<TableData> = [
+    {
+      content: 'Invest in crypto currency with security and accuracy',
+      className: 'td-one',
+    },
+    {
+      content: 'Choose your crypto trading bot depending on your objectives',
+      className: 'td-two',
+    },
+    {
+      content: 'Get real time predictions in the crypto stock market',
+      className: 'td-three',
+    },
+    {
+      content: 'Help us build a decentralized network',
+      className: 'td-four',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
